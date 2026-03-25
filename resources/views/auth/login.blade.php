f09f3b7decd585643c3ca6a5a9cf50a0b7faa6f8
Cristiano Abilio <cristianocafr@gmail.com>
2026-03-25 16:35:08 -0300

    feat: form validations on login and register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,14 +38,26 @@
                                     <div class="pt-0">
                                         <form method="POST" action="{{ route('login') }}" class="my-4">
                                         @csrf
+
+                                            @if (session('error'))
+                                                <div class="alert alert-danger">
+                                                    {{ session('error') }}
+                                                </div>
+                                            @endif
                                             <div class="form-group mb-3">
                                                 <label for="email" name="email" class="form-label">Email address</label>
-                                                <input class="form-control" type="email" id="email" name="email" required="" placeholder="Enter your email">
+                                                <input class="form-control" type="email" id="email" name="email" required="" value="{{ old('email') }}" placeholder="Enter your email">
+                                                @error('email')
+                                                    <small class="text-danger">{{ $message }}</small>
+                                                @enderror
                                             </div>
 
                                             <div class="form-group mb-3">
                                                 <label for="password" class="form-label">Password</label>
                                                 <input class="form-control" type="password" name="password" required="" id="password" placeholder="Enter your password">
+                                                @error('password')
+                                                    <small class="text-danger">{{ $message }}</small>
+                                                @enderror
                                             </div>
 
                                             <div class="form-group d-flex mb-3">

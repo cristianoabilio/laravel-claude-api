f85fa7717e8cc5e1eabca87b3b147375357b6759
Cristiano Abilio <cristianocafr@gmail.com>
2026-03-26 11:27:21 -0300

    feat: updated admin template

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -78,10 +78,8 @@
                                             </div>
                                         </form>
 
-                                        <div class="saprator my-4"><span>or sign in with</span></div>
-
                                         <div class="text-center text-muted mb-4">
-                                            <p class="mb-0">Don't have an account ?<a class='text-primary ms-2 fw-medium' href='{{ route('register') }}'>Sing up</a></p>
+                                            <p class="mb-0">Do You have an account ?<a class='text-primary ms-2 fw-medium' href='{{ route('login') }}'>Sign in</a></p>
                                         </div>
 
                                     </div>
